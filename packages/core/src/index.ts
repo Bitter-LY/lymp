6c92c5d180248b3c3335cc0081fa03575b460753
袁照阳 <alwaysmissly@163.com>
2023-08-14 21:21:38 +0800

    reconfig

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,6 @@
+/// <reference types="@amap/amap-jsapi-types" />
+/// <reference path="../loca.d.ts" />
+
 export { default as Content } from './modules/Content'
 export { default as LabelMarker } from './modules/LabelMarker'
 export { default as Marker } from './modules/Marker'
@@ -6,5 +9,3 @@ export {
   default as PulseLineLayer,
   type PulseLineLayerListener
 } from './modules/PulseLineLayer'
-export * from '@amap/amap-jsapi-types'
-export * from '../loca'
