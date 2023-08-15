55db323ad3e5727e5efcd0577ed6ec7cbe39b215
袁照阳 <alwaysmissly@163.com>
2023-08-15 18:24:10 +0800

    feat/OverlayGroup

--- packages/core/src/modules/OverlayGroup.ts
+++ packages/core/src/modules/OverlayGroup.ts
@@ -1,26 +1,38 @@
 import type Content from './Content'
 import type LabelMarker from './LabelMarker'
 import type Marker from './Marker'
+import type Viewer from './Viewer'
 
-export type OverlayGroupItems = Array<Marker | LabelMarker | Content>
+export type OverlayGroupItem = Marker | LabelMarker | Content
+
+const equals = (a: OverlayGroupItem, b: OverlayGroupItem) => {
+  return a._type === b._type && a._uid === b._uid
+}
 
 export default class OverlayGroup {
-  private _overlays: OverlayGroupItems = []
+  private _overlays: OverlayGroupItem[] = []
+  private _viewer: Viewer | null = null
 
-  constructor(overlays?: OverlayGroupItems) {
+  constructor(overlays?: OverlayGroupItem[]) {
     this._overlays = overlays || []
   }
 
   addOverlay(overlay: Marker | LabelMarker | Content) {
     this._overlays.push(overlay)
+
+    if (!this._viewer) return
+    this._viewer.add(overlay)
   }
 
-  addOverlays(overlays: OverlayGroupItems) {
+  addOverlays(overlays: OverlayGroupItem[]) {
     this._overlays = this._overlays.concat(overlays)
+
+    if (!this._viewer) return
+    this._viewer.add(overlays)
   }
 
   hasOverlay(overlay: Marker | LabelMarker | Content) {
-    return this._overlays.indexOf(overlay) !== -1
+    return this._overlays.some(item => equals(item, overlay))
   }
 
   getOverlays() {
@@ -28,19 +40,30 @@ export default class OverlayGroup {
   }
 
   removeOverlay(overlay: Marker | LabelMarker | Content) {
-    const index = this._overlays.indexOf(overlay)
+    const index = this._overlays.findIndex(item => equals(item, overlay))
     if (index !== -1) {
       this._overlays.splice(index, 1)
     }
+
+    if (this._viewer) {
+      this._viewer.remove(overlay)
+    }
   }
 
-  removeOverlays(overlays: OverlayGroupItems) {
+  removeOverlays(overlays: OverlayGroupItem[]) {
     overlays.forEach(overlay => {
       this.removeOverlay(overlay)
     })
+
+    if (this._viewer) {
+      this._viewer.remove(overlays)
+    }
   }
 
   clearOverlays() {
+    if (this._viewer) {
+      this._viewer.remove(this._overlays)
+    }
     this._overlays = []
   }
 
@@ -54,5 +77,10 @@ export default class OverlayGroup {
     this._overlays.forEach(overlay => {
       overlay.hide()
     })
+  }
+
+  setViewer(viewer: Viewer) {
+    this._viewer = viewer
+    viewer.add(this._overlays)
   }
 }
